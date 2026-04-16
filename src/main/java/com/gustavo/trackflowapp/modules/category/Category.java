b7bc08df80c9b5c92f3eeecb4448b6fb83a0c7e3
Gustavo <sgkenzo212007@gmail.com>
2026-04-16 11:02:58 -0300

    create entity category

---
+++ src/main/java/com/gustavo/trackflowapp/modules/category/Category.java
@@ -0,0 +1,53 @@
+package com.gustavo.trackflowapp.modules.category;
+
+import com.gustavo.trackflowapp.modules.user.User;
+import com.gustavo.trackflowapp.shared.domain.AuditableEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor
+public class Category extends AuditableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private CategoryType type;
+
+    @Column(name = "system_default", nullable = false)
+    private boolean systemDefault;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    public Category(User user, String name, CategoryType type, boolean systemDefault) {
+        this.user = user;
+        this.name = name;
+        this.type = type;
+        this.systemDefault = systemDefault;
+        this.active = true;
+    }
+
+    public void deactivate() {
+        if (!this.systemDefault)
+            this.active = false;
+        else
+            throw new IllegalStateException("system category cannot be deactivated");
+    }
+
+    public void activate() {
+        this.active = true;
+    }
+}
---
+++ src/main/java/com/gustavo/trackflowapp/modules/category/CategoryType.java
@@ -0,0 +1,6 @@
+package com.gustavo.trackflowapp.modules.category;
+
+public enum CategoryType {
+    INCOME,
+    EXPENSE
+}
