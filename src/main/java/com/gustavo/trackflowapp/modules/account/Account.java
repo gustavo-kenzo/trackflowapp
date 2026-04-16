56ad1e756762f2eba04f8fa229273a20b5e3b2c6
Gustavo <sgkenzo212007@gmail.com>
2026-04-16 10:31:49 -0300

    create entity account

---
+++ src/main/java/com/gustavo/trackflowapp/modules/account/Account.java
@@ -0,0 +1,76 @@
+package com.gustavo.trackflowapp.modules.account;
+
+import com.gustavo.trackflowapp.modules.user.User;
+import com.gustavo.trackflowapp.shared.domain.AuditableEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@NoArgsConstructor
+public class Account extends AuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+
+    @Column(name = "opening_balance", nullable = false)
+    private BigDecimal openingBalance;
+
+    @Column(name = "current_balance", nullable = false)
+    private BigDecimal currentBalance;
+
+    @Column(name = "active")
+    private boolean active;
+
+    public Account(User user,
+                   String name,
+                   AccountType type,
+                   BigDecimal openingBalance) {
+        this.user = user;
+        this.name = name;
+        this.type = type;
+        this.openingBalance = openingBalance != null ? openingBalance : BigDecimal.ZERO;
+        this.currentBalance = this.openingBalance;
+        this.active = true;
+    }
+
+    public void credit(BigDecimal amount) {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0)
+            throw new IllegalArgumentException("amount must be positive");
+        this.currentBalance = this.currentBalance.add(amount);
+    }
+
+    public void debit(BigDecimal amount) {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0)
+            throw new IllegalArgumentException("amount must be positive");
+
+//        if (newBalance.compareTo(BigDecimal.ZERO) < 0 && this.type != AccountType.CREDIT_CARD)
+//            throw new IllegalStateException("insufficient funds");
+
+        this.currentBalance = this.currentBalance.subtract(amount);
+    }
+
+    public void deactivate() {
+        this.active = false;
+    }
+
+    public void activate() {
+        this.active = true;
+    }
+}
---
+++ src/main/java/com/gustavo/trackflowapp/modules/account/AccountType.java
@@ -0,0 +1,8 @@
+package com.gustavo.trackflowapp.modules.account;
+
+public enum AccountType {
+    CHECKING,
+    SAVINGS,
+    WALLET,
+    CREDIT_CARD
+}
