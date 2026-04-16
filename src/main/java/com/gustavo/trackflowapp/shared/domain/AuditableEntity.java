02fff42f3189596e70e407d856bba55e0e2d5cd9
Gustavo <sgkenzo212007@gmail.com>
2026-04-16 10:30:55 -0300

    create auditable entity for avoid code repetition

---
+++ src/main/java/com/gustavo/trackflowapp/shared/domain/AuditableEntity.java
@@ -0,0 +1,24 @@
+package com.gustavo.trackflowapp.shared.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class AuditableEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
