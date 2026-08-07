634bdc0aabdc5d45bbca55c649b7da4dd5de91a3
Gustavo <sgkenzo212007@gmail.com>
2026-08-07 15:52:14 -0300

    refactor(config): move JPA auditing to dedicated configuration class

---
+++ src/main/java/com/gustavo/trackflowapp/infra/config/AuditConfig.java
@@ -0,0 +1,9 @@
+package com.gustavo.trackflowapp.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+}
