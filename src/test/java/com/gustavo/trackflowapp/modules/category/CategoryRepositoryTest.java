c384f92e27110cef4a29453e1bcbd508f9ac3a9c
Gustavo <sgkenzo212007@gmail.com>
2026-08-13 17:13:06 -0300

    test(account/category): add @AutoConfigureTestDatabase(replace = NONE) to AccountRepositoryTest and CategoryRepositoryTest.java

--- src/test/java/com/gustavo/trackflowapp/modules/category/CategoryRepositoryTest.java
+++ src/test/java/com/gustavo/trackflowapp/modules/category/CategoryRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
@@ -18,6 +19,7 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class CategoryRepositoryTest {
 
