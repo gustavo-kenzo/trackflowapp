1fbf5395198ccf404c6680b13fca1363d7761cdf
Gustavo <sgkenzo212007@gmail.com>
2026-09-01 15:56:44 -0300

    test(config): configure test environment and properties for contextLoads of TrackFlowAppApplicationTests

--- src/test/java/com/gustavo/trackflowapp/TrackflowappApplicationTests.java
+++ src/test/java/com/gustavo/trackflowapp/TrackflowappApplicationTests.java
@@ -1,9 +1,15 @@
 package com.gustavo.trackflowapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureRestTestClient
+@ActiveProfiles("test")
 class TrackflowappApplicationTests {
 
     @Test
